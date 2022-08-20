9275df4e54e3cc45646d852cde10ceb7f3ea82f4
Allen John <88543723+Allen9994@users.noreply.github.com>
2022-08-20 16:53:25 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,24 +1,24 @@
 import random
-i = 0
+flag = True
 print("WELCOME TO PASSWORD GENERATOR!\n")
-while(i == 0):
+while(flag):
     m = input("Enter level of password difficulty\n(1 for alphanumeric 2 for special characters)\n")
     if len(m) == 0:
         print("Try again")
     elif ord(m) >= 49 and ord(m) <= 50:
-        i = 1
-i = 0
-m = int(m)
+        flag = False
+        m = int(m)
+flag = True
 if m == 1:
     li = [0,1,2]
 else:
     li = [0,1,2,3]
-while(i == 0):
+while(flag):
     n = input("Enter length of the password (minimum : 8 maximum : 20)")
     if len(n) == 0:
         print("Try again")
     elif int(n)>=8 and int(n)<=20:
-        i = 1
+        flag = False
 n = int(n)
 for x in range(n-len(li)):
     li.append(random.choice(li))
